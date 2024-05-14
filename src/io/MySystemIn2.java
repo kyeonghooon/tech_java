45980cc48ca77b3d30fced61207ab3e536a72c65
kyeonghooon <gibbon0510@gmail.com>
2024-05-14 20:03:40 +0900

    I/O

---
+++ src/io/MySystemIn2.java
@@ -0,0 +1,34 @@
+package io;
+
+import java.io.IOException;
+
+public class MySystemIn2 {
+
+	public static void main(String[] args) {
+		
+		// 키보드에서 데이터를 응용 프로그램 안으로 넣기
+		System.out.println("알파벳 하나를 쓰고 Enter를 누르세요");
+		
+		// 바이트 동작 --> 0과 1로 구성
+		
+		// 정수 -> byte, short, int, long
+		
+		int i;
+		// 한 바이트씩 키보드의 값을 읽어라
+		
+		try {
+			
+			while( (i = System.in.read()) != '\n') {
+				System.out.println("i : " + i);
+				System.out.println("(char)i : " + (char)i);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	} // end of main
+
+}
